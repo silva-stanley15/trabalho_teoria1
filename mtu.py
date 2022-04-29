2d64b25f6afc43ded351caeee0a7da8f14d5c7d3
gustavohmoises <gustavomoises@deltainova.com.br>
2022-04-29 19:13:35 -0300

    ajst transicao e inclusao código MTU

---
+++ mtu.py
@@ -0,0 +1,73 @@
+import csv
+
+def codificar(entrada):
+    if (entrada == '0'):
+        return '1'
+    elif (entrada == '1'):
+        return '11'
+    elif (entrada == 'B'):
+        return '111'
+
+def decodificar(entrada):
+    if (entrada == '1'):
+        return '0'
+    elif (entrada == '11'):
+        return '1'
+    elif (entrada == '111'):
+        return 'B'
+
+def MTU():
+    with open('exemplo2.csv', 'r') as file:
+        reader = csv.reader(file)
+        entrada = []
+        for row in reader:
+            entrada = row
+
+    binarios = entrada[0].split(';')
+
+    fita1 = '00010101010110010110101101100101110110111011001101011010110011011011011011001101110111011101001110101110110100111011011110101001110111011111101110110011110101111010100111101101111011010011110111011111011101001111101010110110011111011011111010100111110111010110110011111101101111110111011001111110111011111110111011000'
+    fita2 = '1' # estado q0 codificado
+    fita3 = binarios[0] + 'B' + binarios[1] + 'b' # entrada w
+
+    transicoes = fita1.split('000')
+    transicoes = transicoes[1].split('00')
+
+    cabeca_leitura = 0
+
+    rodando = True
+    while(rodando):
+        estado = fita2
+        simbolo = fita3[cabeca_leitura]
+        
+        transicao_encontrada = False
+        for transicao in transicoes:
+            componentes = transicao.split('0')
+            if (componentes[0] == estado and componentes[1] == codificar(simbolo)): #qi == estado && x == simbolo
+                transicao_encontrada = transicao
+                break
+
+        if (transicao_encontrada):
+            componentes = transicao.split('0')
+
+            fita2 = componentes[2] # qj
+            fita3 = fita3[:cabeca_leitura] + decodificar(componentes[3]) + fita3[cabeca_leitura+1:] #y
+
+            print('Estado:' +estado)
+            print('Simbolo antigo:' +simbolo)
+            print('Simbolo novo:' +decodificar(componentes[3]))
+            print('Fita:' +fita3)
+            print('--------')
+
+
+            if (componentes[4] == '1'): # L
+                cabeca_leitura = cabeca_leitura - 1
+            elif (componentes[4] == '11'): # R
+                cabeca_leitura = cabeca_leitura + 1
+
+        else:
+            rodando = False
+
+    
+    return fita3
+
+MTU()
